eb16342a70d7446351bae08e869688e268b12796
Tan Juen Woo <tanjuenwoo@gmail.com>
2022-09-21 23:45:51 +0800

    Add app researchs and set up initial files needed

--- research_backend/settings.py
+++ research_backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework_simplejwt.token_blacklist',
     'accounts.apps.AccountsConfig',
+    'researchs.apps.ResearchsConfig',
 ]
 
 MIDDLEWARE = [
--- research_backend/urls.py
+++ research_backend/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls', namespace='accounts')),
+    path('research/', include('researchs.urls', namespace='researchs')),
 ]
